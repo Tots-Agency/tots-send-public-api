6dca76a4afdb6e27561d5f742974399a3d1db930
Matias Camiletti <matias.camiletti@gmail.com>
2023-07-20 18:25:59 -0300

    Add main class.

---
+++ src/TotsSend.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TotsSend;
+
+use GuzzleHttp\Psr7\Request;
+
+class TotsSend 
+{
+    /**
+     * URL de la API
+     */
+    const BASE_URL = 'https://api.send.tots.agency/';
+    /**
+     *
+     * @var string
+     */
+    protected $apiKey = '';
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $guzzle;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function send($email, $template, $params = [])
+    {
+        return $this->generateRequest('POST', 'api/send', [
+            'email' => $email,
+            'template' => $template,
+            'vars' => $params
+        ]);
+    }
+
+    protected function generateRequest($method, $path, $params = null)
+    {
+        $body = null;
+        if($params != null){
+            $body = json_encode($params);
+            $body['api_key'] = $this->apiKey;
+        }
+
+        $request = new Request(
+            $method, 
+            self::BASE_URL . $path, 
+            [
+                'Content-Type' => 'application/json',
+            ], $body);
+
+        $response = $this->guzzle->send($request);
+        if($response->getStatusCode() == 200){
+            return json_decode($response->getBody()->getContents());
+        }
+
+        return null;
+    }
+}
